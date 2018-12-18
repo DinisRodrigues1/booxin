599f556b1d2f642c26d497d56163fbfeb72bed2f
João Pantaleão <jpantaleao@ubiwhere.pt>
2018-12-18 09:36:48 +0000

    added modules

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div className="App">
+        <Navbar color="dark">
+          <NavbarBrand href="/">boox</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/login">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/sobre">Sobre</NavLink>
+              </NavItem>
+            </Nav>
+        </Navbar>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
