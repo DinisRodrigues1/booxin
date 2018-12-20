a205d40697eaf782782b99f1c92af593037cb131
DinisRodrigues1 <joaodtrodrigues@ua.pt>
2018-12-20 17:12:45 +0000

    changes to footer and some scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './App.css';
+import './App.scss';
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import Home from './components/Home'
