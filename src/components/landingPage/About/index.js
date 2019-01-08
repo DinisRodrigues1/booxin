e46258b0fa9574720241ce013a48eb5f0784b30c
João Pantaleão <jpantaleao@ubiwhere.pt>
2019-01-08 20:26:08 +0000

    added sobre + styles

--- src/components/landingPage/About/index.js
+++ src/components/landingPage/About/index.js
@@ -4,7 +4,11 @@ class About extends Component {
     render(){
         return(
             <Fragment>
-                <h1>About goes here</h1>
+                <h1 className="margin">Trabalho realizado por:</h1>
+                <div className="margin">
+                    <a href="https://github.com/DinisRodrigues1" style={{ textDecoration: 'none' }}>Dinis</a> Rodrigues<br/>
+                    João <a href="http://pantaleao.solutions" style={{ textDecoration: 'none' }}>Pantaleão</a>
+                </div>
             </Fragment>
         )
     }
